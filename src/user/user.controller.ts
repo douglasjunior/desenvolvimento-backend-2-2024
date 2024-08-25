ddcb42fa3295c0afffd9aa13e21faddeb8ec5efe
Douglas Nassif Roma Junior <nassifrroma@gmail.com>
2024-08-25 16:13:40 -0300

    Aula 02

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,48 +1,37 @@
 import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { UserService } from './user.service';
 
 // http://localhost:3000/user
 @Controller('user')
 export class UserController {
 
+  constructor(private userService: UserService) {}
+
   // GET http://localhost:3000/user
   @Get('/')
   getUsers() {
-    return [
-      {
-        id: 1,
-        name: 'Douglas'
-      },
-      {
-        id: 2,
-        name: 'Bruna'
-      }
-    ]
+    return this.userService.getUsers();
   }
 
   // GET http://localhost:3000/user/2
   @Get('/:userId')
   getUserById(@Param('userId') userId: string) {
-    return {
-      id: Number(userId),
-      name: 'Douglas'
-    }
+    return this.userService.getUserById(Number(userId));
   }
 
   // POST http://localhost:3000/user
   // BODY: { "name": "Douglas", "age": 35 }
   @Post('/')
   createUser(@Body() createUserDto: CreateUserDto) {
-    console.log('user created', createUserDto);
-    return createUserDto;
+    return this.userService.createUser(createUserDto);
   }
 
   // PATCH http://localhost:3000/user/2
   // BODY: { "name": "Douglas" } ou { "age": 35 } ou { "name": "Douglas", "age": 35 }
   @Patch('/:userId')
   updateUser(@Param('userId') userId: string, @Body() updateUserDto: UpdateUserDto) {
-    console.log('user updated', updateUserDto);
-    return updateUserDto;
+    return this.userService.updateUser(Number(userId), updateUserDto);
   }
 }
---
+++ src/user/user.service.ts
@@ -0,0 +1,54 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+@Injectable()
+export class UserService {
+
+  private lastId = 2;
+  private users = [
+    {
+      id: 1,
+      name: 'Douglas',
+      age: 35
+    },
+    {
+      id: 2,
+      name: 'Maria',
+      age: 25
+    }
+  ];
+
+  updateUser(userId: number, updateUserDto: UpdateUserDto) {
+    const userIndex = this.users.findIndex(user => user.id === userId);
+    if (userIndex < 0) {
+      throw new NotFoundException('User not found');
+    }
+    this.users[userIndex] = {
+      ...this.users[userIndex],
+      ...updateUserDto
+    };
+    return this.users[userIndex];
+  }
+
+  createUser(createUserDto: CreateUserDto) {
+    const newUser = {
+      ...createUserDto,
+      id: ++this.lastId
+    };
+    this.users.push(newUser);
+    return newUser;
+  }
+
+  getUserById(userId: number) {
+    const user = this.users.find(user => user.id === userId);
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    return user;
+  }
+
+  getUsers() {
+    return this.users;
+  }
+}
