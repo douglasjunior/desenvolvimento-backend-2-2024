096bfdb55c8b9320363c15b553329e14865d529f
Douglas Nassif Roma Junior <nassifrroma@gmail.com>
2024-08-27 20:52:17 -0300

    Aula 03

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+// import { loggerMiddleware } from './common/middleware/logger-fn.middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // app.use(loggerMiddleware);
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
