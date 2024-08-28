1984e5f70585ebe6666a28f3b61a147dd9f98f21
Douglas Nassif Roma Junior <nassifrroma@gmail.com>
2024-08-27 21:13:40 -0300

    Aula 03

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,19 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as path from 'path';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: path.join('database', 'db.sqlite'),
+      entities: [path.join(__dirname, '**', '*.entity.{ts,js}')],
+      synchronize: true,
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService]
 })
