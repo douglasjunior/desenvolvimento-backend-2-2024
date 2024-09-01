ddd14fa2a83cd15afe825786077201b1c6a48c20
Douglas Nassif Roma Junior <nassifrroma@gmail.com>
2024-09-01 14:22:20 -0300

    Aula 06

--- src/user/user.controller.spec.ts
+++ src/user/user.controller.spec.ts
@@ -1,12 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserController } from './user.controller';
+import { UserService } from './user.service';
+import { User } from './data/user.entity';
 
 describe('UserController', () => {
   let controller: UserController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        TypeOrmModule.forRoot({
+          type: 'sqlite',
+          database: ':memory:',
+          entities: [User],
+          synchronize: true,
+        }),
+        TypeOrmModule.forFeature([User])
+      ],
       controllers: [UserController],
+      providers: [UserService],
     }).compile();
 
     controller = module.get<UserController>(UserController);
@@ -15,4 +28,54 @@ describe('UserController', () => {
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+  it('deve criar um novo usuário', async () => {
+    const newUser = await controller.createUser({
+      name: 'Douglas',
+      email: 'user@mail.com',
+      password: 'senha123',
+      age: 35
+    });
+
+    expect(newUser).toEqual({
+      name: 'Douglas',
+      email: 'user@mail.com',
+      password: 'senha123',
+      age: 35,
+      id: 1,
+    });
+  });
+
+  it('deve retornar os usuários criados', async () => {
+    await controller.createUser({
+      name: 'Douglas',
+      email: 'user@mail.com',
+      password: 'senha123',
+      age: 35
+    });
+    await controller.createUser({
+      name: 'Bruna',
+      email: 'user2@mail.com',
+      password: 'senha123',
+      age: 35
+    });
+
+    const users = await controller.getUsers();
+    expect(users).toEqual([
+      {
+        name: 'Douglas',
+        email: 'user@mail.com',
+        age: 35,
+        id: 1,
+      },
+      {
+        name: 'Bruna',
+        email: 'user2@mail.com',
+        age: 35,
+        id: 2,
+      }
+    ]);
+  });
 });
+
+
